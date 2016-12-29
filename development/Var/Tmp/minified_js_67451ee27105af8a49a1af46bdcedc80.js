9668f6db40beac0d28a4dfd83575668b0cfca05e
Tom Flídr <tomflidr@gmail.com>
2016-12-30 00:30:03 +0100

    RELEASE 1.0.0

---
+++ development/Var/Tmp/minified_js_67451ee27105af8a49a1af46bdcedc80.js
@@ -0,0 +1,2 @@
+/* /static/js/Front.js */
+Class.Define('Front',{Extend:Module,Constructor:function(){this.parent();console.log(document.getElementsByTagName("h1")[0].innerHTML);}});window.front=new Front();
