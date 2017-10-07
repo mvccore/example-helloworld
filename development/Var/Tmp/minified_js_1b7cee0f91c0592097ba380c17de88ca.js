0208c3218b54a84111bcf5179942043c77bf16ac
Tom Flídr <tomflidr@gmail.com>
2017-10-07 20:07:54 +0200

    - version alighment to v4.2.0

---
+++ development/Var/Tmp/minified_js_1b7cee0f91c0592097ba380c17de88ca.js
@@ -0,0 +1,4 @@
+
+/* /static/js/Front.js */
+Class.Define('Front',{Extend:Module,Constructor:function(){this.parent();console.log(document.getElementsByTagName("h1")[0].innerHTML);}});if(!console){var console={log:function(){}}}
+window.front=new Front();
