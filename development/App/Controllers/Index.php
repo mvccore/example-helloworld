867a4b4529e4443c1d860893e2e7b21714525cf7
Tom Flídr <tomflidr@gmail.com>
2018-05-09 18:34:43 +0200

    release 5 alpha

--- development/App/Controllers/Index.php
+++ development/App/Controllers/Index.php
@@ -4,13 +4,21 @@ namespace App\Controllers;
 
 class Index extends Base
 {
-    public function IndexAction () {
+    public function IndexAction(){
 		$this->view->Title = "MvcCore Hello World Example";
 		$this->view->Version = \MvcCore\Interfaces\IApplication::VERSION;
-		$this->view->CompileMode =  $this->application->GetCompiled();
-    }
-	public function NotFoundAction () {
-		$this->view->Title = "Error 404 - requested page not found.";
-		$this->view->Message = $this->request->GetParam('message');
+		$this->view->CompileMode = $this->application->GetCompiled();
+	}
+	public function NotFoundAction(){
+		$this->ErrorAction();
+	}
+	public function ErrorAction(){
+		$code = $this->response->GetCode();
+		$message = $this->request->GetParam('message', 'a-zA-Z0-9_;, /\\\-\@\:');
+		$message = preg_replace('#`([^`]*)`#', '<code>$1</code>', $message);
+		$this->view->Title = "Error $code";
+		$this->view->Message = $message;
+		$this->Render('error');
 	}
 }
+
